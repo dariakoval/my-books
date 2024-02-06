649fcc008a5f463c1eec4c4f8eda1f1b285fa09a
dariakoval <dasharibkavmore666@mail.ru>
2024-02-06 21:17:11 +0400

    add RequestUtil.java

---
+++ src/main/java/org/example/util/RequestUtil.java
@@ -0,0 +1,25 @@
+package org.example.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestUtil {
+    public static String getId(HttpServletRequest request) {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            return null;
+        }
+        String[] pathParts = pathInfo.split("/");
+        return ArrayUtils.get(pathParts, 1, null);
+    }
+
+    public static String getAction(HttpServletRequest request) {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            return "list";
+        }
+        String[] pathParts = pathInfo.split("/");
+        return ArrayUtils.get(pathParts, 2, getId(request));
+    }
+}
